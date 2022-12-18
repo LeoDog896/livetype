76f7c430b933a4f2648ffd35125bbee2c1e414ba
willuhm-js <willuhmjs@gmail.com>
2022-12-17 21:24:50 -0500

    most loved framework?

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { Server } from 'socket.io';
+
+const plugin =
+	{
+		name: 'sveltekit-socket-io',
+		configureServer(server) {
+			const io = new Server(server.httpServer);
+
+			// This is located in the svelte config (see above "Socket.IO stuff goes here")
+			io.on('connection', (socket) => {
+				// Generate a random username and send it to the client to display it
+				let username = `User ${Math.round(Math.random() * 999999)}`;
+				socket.emit('name', username);
+
+				// Receive incoming messages and broadcast them
+				socket.on('message', (message) => {
+					io.emit('message', {
+						from: username,
+						message: message,
+						time: new Date().toLocaleString()
+					});
+				});
+			});
+
+			console.log('SocketIO injected');
+		}
+	}
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), plugin]
 };
 
 export default config;
